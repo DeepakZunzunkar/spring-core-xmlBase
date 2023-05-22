e56ba0a529209fa6823a790499d30e1d42b7fd06
DeepakJavaDev <dzunzunkar@gmail.com>
2023-05-22 10:08:33 +0530

    basics of xml config

--- src/main/java/com/dz/app/App.java
+++ src/main/java/com/dz/app/App.java
@@ -1,5 +1,8 @@
 package com.dz.app;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/dz/app/config.xml");
+        Employee employee1=(Employee)context.getBean("e1");
+        System.out.println(employee1);
+        
+        Employee employee2=(Employee)context.getBean("e2");
+        System.out.println(employee2);
+        
+        Employee employee3=(Employee)context.getBean("e3");
+        System.out.println(employee3);
     }
 }
